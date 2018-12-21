84def66351e98a7ef3a8c71a9296945519d72709
sai <git clone https://SaiVutukuri@bitbucket.org/SaiVutukuri/demo.git>
2018-12-21 17:53:21 +0530

    Commit3

---
+++ project2frontend/WebContent/services/blogservice.js
@@ -0,0 +1,44 @@
+/**
+ * BlogService
+ */
+
+app.factory('BlogService',function($http){
+	var blogService={}
+	var BASE_URL="http://localhost:8086/project2middleware"
+		
+	blogService.addBlog=function(blog){
+		return $http.post(BASE_URL + "/addblogpost",blog)
+	}
+	
+	blogService.blogsWaitingForApproval=function(){
+		return $http.get(BASE_URL + "/blogswaitingforapproval")
+	}
+	
+	blogService.blogsApproved=function(){
+		return $http.get(BASE_URL + "/blogsapproved")
+	}
+	
+	blogService.getBlog=function(blogId){
+		return $http.get(BASE_URL + "/getblog/"+blogId)
+	}
+	blogService.approveBlogPost=function(blogPostId){
+		return $http.put(BASE_URL + "/approveblogpost/"+blogPostId)
+	}
+	blogService.rejectBlogPost=function(blogPostId,rejectionReason){
+		if(rejectionReason == undefined)
+			{
+			rejectionReason='Not Mentioned By Admin'
+			}
+		return $http['delete'](BASE_URL + "/rejectblogpost/"+blogPostId+"/"+rejectionReason)
+	}
+	blogService.updateBlogPost=function(blogPost){
+		return $http.put(BASE_URL + "/update",blogPost)
+	}
+	blogService.blogswaitingForApprovalPostedByUser=function(){
+		return $http.get(BASE_URL + "/getblogbyemail")
+	}
+	blogService.blogswaitingForApprovalPostedByUser1=function(){
+		return $http.get(BASE_URL + "/getblogbyemail1")
+	}
+	return blogService
+})
